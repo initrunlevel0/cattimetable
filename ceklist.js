cb9c3c74cf2c6d25c1f7f945acf42fc58f66ba2b
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2024-11-29 10:23:32 +0800

    perbaikan ceklist

--- ceklist.js
+++ ceklist.js
@@ -1,8 +1,9 @@
 ceklist_start = {
-    "startup": [],
+    "startup": ["sudah sarapan"],
     "registrasi": ["create pin, status: off, izin pin: on", "notify ke petugas registrasi"],
     "enter": ["hide live score", "siapkan live score sesi selanjutnya (masih hidden)"],
-    "ujian": ["this sesi, izin pin: off, status: on", "show live score", "notify pin sesi ke peserta", "pastikan peserta login semua", "pastikan headcount", "pastikan tandai tidak hadir"]
+    "ujian": ["this sesi, izin pin: off, status: on", "show live score", "notify pin sesi ke peserta", "pastikan peserta login semua", "pastikan headcount", "pastikan tandai tidak hadir"],
+    "end": ["pastikan ruangan kosong"]
 };
 
 
@@ -10,6 +11,7 @@ ceklist_end = {
     "startup": ["siapkan obs live score", "siapkan cctv zoom", "stream ke youtube", "cek rekaman cctv kemarin", "backup rekaman cctv", "upload ceklist-harian"],
     "registrasi": ["this sesi, izin pin: off, status: on", "notify pin sesi ke petugas BKN"],
     "enter": ["mulai pengarahan"],
-    "ujian": ["pastikan peserta semua selesai", "this sesi, status: off, izin pin: off", "log ke dashboard", "upload absensi", "upload hasil"]
+    "ujian": ["pastikan peserta semua selesai", "this sesi, status: off, izin pin: off", "log ke dashboard", "upload absensi", "upload hasil"],
+    "end": ["ba harian", "matikan live score", "matikan pc", "matikan ac/lampu", "segel ruang dan foto", "foto segel"]
 };
 
